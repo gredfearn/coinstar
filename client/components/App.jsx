671f2ede3c60718c5b9a6e8d31aa0593d1227953
gredfearn <grantredfearn@gmail.com>
2018-01-23 11:19:15 -0600

    fetching currencies

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import request from 'superagent';
 
 export default class App extends React.Component {
-  render() {
+	constructor() {
+		super();
+		this.state.currencies = ["BTC", "ETH", "NEO", "XLM", "XRP", "ARK"];
+		this.state.market = {};
+	}
+
+
+	fetchPrice(coin) {
+		const url = `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD,EUR`;
+		request
+			.get(url)
+			.set('accept', 'json')
+			.end((err, res) => {
+				if(err) {
+					console.log(`Error: ${err.message}`);
+					return null;
+				}
+
+				let market = this.state.market;
+				market[coin] = res.body["USD"];
+				this.setState({ market });
+			})
+	}
+
+	batchCalls(coins) {
+		let promiseArray = [];
+		coins.forEach(coin => {
+			promiseArray.push(this.fetchPrice(coin));
+		})
+
+		return Promise.all(promiseArray);
+	}
+
+	componentWillMount() {
+		this.fetchPrice = this.fetchPrice.bind(this);
+		this.batchCalls = this.batchCalls.bind(this);
+		
+		this.batchCalls(this.state.currencies);
+	}
+
+	render() {
+		console.log('this.state!', this.state);
     return (
-      <div style={{textAlign: 'center'}}>
+      <div>
         <h1>Hello World</h1>
       </div>
     );
