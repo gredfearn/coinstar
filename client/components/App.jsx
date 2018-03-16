37716532050355797a3d97547b54f5a6d2404a18
gredfearn <grantredfearn@gmail.com>
2018-03-16 13:18:56 -0500

    fixed tabs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,60 +2,65 @@ import React from 'react';
 import request from 'superagent';
 
 export default class App extends React.Component {
-	constructor() {
-		super();
-		this.state.currencies = ["BTC", "ETH", "NEO", "XLM", "XRP", "ARK"];
-		this.state.market = this.buildDefaultState(this.state.currencies);
-	}
+  constructor() {
+    super();
+    this.state.currencies = ["BTC", "ETH", "NEO", "XLM", "XRP", "ARK"];
+    this.state.market = this.buildDefaultState(this.state.currencies);
+  }
 
-	buildDefaultState(coins) {
-		let state = {};
-		coins.forEach(coin => {
-		const	obj = {
-				name: coin,
-				value: null,
-				up: true
-			};
+  buildDefaultState(coins) {
+    let state = {};
+    coins.forEach(coin => {
+      const obj = {
+        name: coin,
+        value: null,
+        up: true
+      };
 
-			state[coin] = obj;
-		})
+      state[coin] = obj;
+    });
 
-		return state;
-	}
+    return state;
+  }
 
-	fetchPrice(coin) {
-		const url = `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD,EUR`;
-		request
-			.get(url)
-			.set('accept', 'json')
-			.end((err, res) => {
-				if(err) {
-					console.log(`Error: ${err.message}`);
-					return null;
-				}
+  fetchPrice(coin) {
+    const url = `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD,EUR`;
+    request
+      .get(url)
+      .set('accept', 'json')
+      .end((err, res) => {
+        if(err) {
+            console.log(`Error: ${err.message}`);
+            return null;
+        }
 
-				let market = this.state.market;
-				market[coin].up =  market[coin].value < res.body["USD"]
-				market[coin].value = res.body["USD"];
-				this.setState({ market });
-			})
-	}
+        let market = this.state.market;
+        market[coin].up =  market[coin].value < res.body["USD"]
+        market[coin].value = res.body["USD"];
+        this.setState({ market });
+      })
+}
 
-	batchCalls(coins) {
-		let promiseArray = [];
-		coins.forEach(coin => {
-			promiseArray.push(this.fetchPrice(coin));
-		})
+  batchCalls(coins) {
+    let promiseArray = [];
+    coins.forEach(coin => {
+        promiseArray.push(this.fetchPrice(coin));
+    });
 
-		return Promise.all(promiseArray);
-	}
+    return Promise.all(promiseArray);
+  }
 
-	componentWillMount() {
-		this.batchCalls(this.state.currencies);
-	}
+  componentWillMount() {
+    this.batchCalls(this.state.currencies);
+    this.retrievePricesInterval(10000);
+  }
 
-	render() {
-		console.log('this.state!', this.state);
+  retrievePricesInterval(interval) {
+      setInterval(interval, this.batchCalls(this.state.currencies))
+  }
+
+  render() {
+    console.log('this.state!', this.state);
     return (
       <div>
         <h1>Hello World</h1>
